766f4c85d9781bd988f50d671363e388e5ee60be
sijankasalawat <sijankasalawat3339@gmail.com>
2023-09-10 16:59:56 +0545

    my portfolio

---
+++ my-react-app/src/Dashboard.js
@@ -0,0 +1,23 @@
+function Dashboard(){
+    return(
+        <>
+        <header class="d-flex justify-between align-center bg-primary" >
+            <div class="logo">
+                <h3>
+                    Portfolio
+                </h3>
+
+            </div>
+            <div>
+
+            </div>
+
+        </header>
+        <div class="container">
+
+        </div>
+        </>
+
+    );
+
+}export default Dashboard;
