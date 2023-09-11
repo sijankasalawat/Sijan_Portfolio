d0a9e8ba99c50b2a2ff7680d4310706b5b39fd34
sijankasalawat <sijankasalawat3339@gmail.com>
2023-09-11 15:38:12 +0545

    portfolio

--- my-react-app/src/Dashboard.js
+++ my-react-app/src/Dashboard.js
@@ -1,19 +1,148 @@
 function Dashboard(){
     return(
         <>
-        <header class="d-flex justify-between align-center bg-primary" >
-            <div class="logo">
-                <h3>
-                    Portfolio
-                </h3>
+        <header className="d-flex justify-between align-center" >
+           
+                <div>
+                <div className="logo">
+                </div>
+                <h2>SIJAN.K</h2>
+                </div>
+                <div className="nav ">
+                    <ul className="d-flex ">
+                        <li><a>Home</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Contact</a></li>
+                        <li><a>Explore</a></li>
+                    </ul>
 
-            </div>
-            <div>
+                </div>
 
-            </div>
+         
+        
 
         </header>
-        <div class="container">
+        <div className="main-wrapper ">
+            <div className="wrapper-1 d-lg-flex d-md-flex d-sm-block ">
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    {/* profile image
+                    <img></img> */}
+
+                </div>
+            <div className="col-lg-6 col-md-6 col-sm-12 px-2">
+                <h3>
+                I’m Sijan Kasalawat.
+                    A Fontend Developer
+                </h3>
+                <p>I’m probably the most passionate designer you will ever get to work with. If you have a great project that needs some amazing skills, I’m your guy.</p>
+
+             </div>
+            </div>
+            <div className="wrapper-2 px-2">
+                <div >
+                    <p>
+                    WORK EXPERIENCE
+                    </p>
+                    <h2>
+                    Companies I have worked for in the past. 
+                    </h2>
+                </div>
+                <div className="col-12">
+                    <div className="row">
+                        <div className="col-lg-4 col-md-6 col-sm-12 p-2">
+                            <h1>
+                                01
+                            </h1>
+                            <h4>
+                            Google, Interaction Designer
+                            </h4>
+                            <p>
+                            I currently am the lead designer on the interaction design team for Google Play. 
+                            </p>
+
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 p-2">
+                        <h1>
+                                01
+                            </h1>
+                            <h4>
+                            Google, Interaction Designer
+                            </h4>
+                            <p>
+                            I currently am the lead designer on the interaction design team for Google Play. 
+                            </p>
+
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 p-2">
+                        <h1>
+                                01
+                            </h1>
+                            <h4>
+                            Google, Interaction Designer
+                            </h4>
+                            <p>
+                            I currently am the lead designer on the interaction design team for Google Play. 
+                            </p>
+
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+            <div className="wrapper-3">
+                <div className="col-lg-6 col-md-6 col-sm-12">
+<h2>Philosophy & values</h2><p>I think everyone wants the same thing - relationship with humanity, peace with the metaphysical, and experience with the universe. I try to grasp these things with my values: authenticity, creativity, & hospitality.</p>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    
+                    </div>
+    
+
+            </div>
+            <div className="wrapper-4 col-12">
+                <div className="row">
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <h1>
+                        Skillset
+                        </h1>
+                        <p>With skills in over 4 different fields of design, I am the perfect person to hire when it comes to a full fledged project. Whatever your needs are, I can pretty much take on any challenge.</p>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <div className="col-12">
+                            <div class="row">
+         
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                            <h4>Product Design</h4>
+                            <p>
+                            Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.
+                            </p>
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                            <h4>Product Design</h4>
+                            <p>
+                            Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.
+                            </p>
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                            <h4>Product Design</h4>
+                            <p>
+                            Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.
+                            </p>
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                            <h4>Product Design</h4>
+                            <p>
+                            Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.
+                            </p>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
 
         </div>
         </>
